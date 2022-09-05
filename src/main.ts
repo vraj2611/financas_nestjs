b2498c741f155ef0b887ba15abd80a0ad5f14aaa
Vilmar Rangel <vraj2611@gmail.com>
2022-09-05 00:49:35 +0000

    changed PORT
    Signed-off-by: Vilmar Rangel <vraj2611@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT || 8976);
   console.log(`App on: ${await app.getUrl()}`);
 }
 bootstrap();
